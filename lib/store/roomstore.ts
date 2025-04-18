d0feb6a3ff6805996d45cd3c4821d7d943f4c27b
JAGADEESHWARAN20 <jagadeeshwaransp5@gmail.com>
2025-04-18 12:03:09 +0530

    new updates

--- lib/store/roomstore.ts
+++ lib/store/roomstore.ts
@@ -4,13 +4,17 @@ import { IRoom } from '@/lib/types/rooms';
 interface RoomState {
      rooms: IRoom[];
      selectedRoom: IRoom | null;
+     defaultRoom: IRoom | null;
      setRooms: (rooms: IRoom[]) => void;
      setSelectedRoom: (room: IRoom | null) => void;
+     setDefaultRoom: (room: IRoom | null) => void;
 }
 
 export const useRoomStore = create<RoomState>((set) => ({
      rooms: [],
      selectedRoom: null,
+     defaultRoom: null,
      setRooms: (rooms) => set({ rooms }),
-     setSelectedRoom: (room) => set({ selectedRoom: room })
+     setSelectedRoom: (room) => set({ selectedRoom: room }),
+     setDefaultRoom: (room) => set({ defaultRoom: room })
 }));
