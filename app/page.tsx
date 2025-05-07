b157d056cf57db2aa7e6c9146b83fc39da407915
JAGADEESHWARAN20 <jagadeeshwaransp5@gmail.com>
2025-05-07 19:02:07 +0530

    Added files

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,22 @@
-import React, { Suspense } from "react";
+import React from "react";
 import ChatHeader from "@/components/ChatHeader";
 import { supabaseServer } from "@/lib/supabase/server";
 import InitUser from "@/lib/store/InitUser";
 import ClientChatContent from "@/components/ClientChatContent";
-import { Skeleton } from "@/components/ui/skeleton";
 
 export default async function Page() {
 	const supabase = supabaseServer();
 	const { data } = await supabase.auth.getSession();
 
 	return (
-		<Suspense fallback={<Skeleton className="w-full h-screen bg-gray-900" />}>
-			<div className="flex h-screen bg-gray-900 text-white">
-				{/* Sidebar will be handled in ChatHeader */}
-				<div className="flex-1 flex flex-col">
+		<>
+			<div className="max-w-3xl mx-auto md:py-10 h-screen">
 					<ChatHeader user={data.session?.user} />
-					<div className="flex-1 overflow-auto">
-						<ClientChatContent user={data.session?.user} />
-					</div>
+				<div className="h-full border rounded-md flex flex-col relative">
+					<ClientChatContent user={data.session?.user} />
 				</div>
-				<InitUser user={data.session?.user} />
 			</div>
-		</Suspense>
+			<InitUser user={data.session?.user} />
+		</>
 	);
 }
