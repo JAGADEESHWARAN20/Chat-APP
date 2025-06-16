3a92562e1ae402d11dc82722f32848a05caa7449
JAGADEESHWARAN20 <jagadeeshwaransp5@gmail.com>
2025-06-16 11:22:41 +0530

    notification toast

--- components/ResponsiveToaster.tsx
+++ components/ResponsiveToaster.tsx
@@ -50,7 +50,7 @@ export function ResponsiveToaster() {
                }}
                visibleToasts={5}
                richColors={false}
-               closeButton
+               
           />
      );
 }
