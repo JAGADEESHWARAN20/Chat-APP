43c712c3672fc12bcdc1d505234b22f75e77fd50
JAGADEESHWARAN20 <jagadeeshwaransp5@gmail.com>
2025-07-24 21:47:12 +0530

    first commit

--- components/ChatHeader.tsx
+++ components/ChatHeader.tsx
@@ -870,13 +870,12 @@ export default function ChatHeader({ user }: { user: SupabaseUser | undefined })
             side="bottom"
             align="center"
             sideOffset={0}
-            collisionPadding={{ left: 16,right:16 }}
             className="
               !w-[min(32em,96vw)]
               !h-[min(40em,90vh)]
               md:!w-[32em]
               md:!h-[40em]
-              mx-[3vw]
+              mr-[1em]
               mt-[1em]
               mb-[2vh]
               bg-gray-800/30
