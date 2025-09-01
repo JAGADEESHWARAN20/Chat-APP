092cefd04ebd9c195b5be3fed00ec0c4c7d04134
JAGADEESHWARAN20 <jagadeeshwaransp5@gmail.com>
2025-09-01 22:15:51 +0530

    Fix async cookies and params issues for Next.js 15+ compatibility

--- components/ClientChatContent.tsx
+++ components/ClientChatContent.tsx
@@ -13,7 +13,7 @@ export default function ClientChatContent({ user }: { user: SupabaseUser | undef
   const { selectedRoom } = state;
 
   return (
-    <div className="h-[90vh] w-full flex flex-col">
+    <div className="h-[90vh] w-full mx-auto flex flex-col">
       <ChatHeader user={user} />
       <div className="flex-1 flex flex-col">
         {user && selectedRoom ? (
