26a37f1ae3384c3337c4d0f496683b87029c00c7
JAGADEESHWARAN20 <jagadeeshwaransp5@gmail.com>
2025-09-01 19:48:13 +0530

    Fix async cookies and params issues for Next.js 15+ compatibility

--- components/ClientChatContent.tsx
+++ components/ClientChatContent.tsx
@@ -9,28 +9,26 @@ import { User as SupabaseUser } from "@supabase/supabase-js";
 import ChatHeader from "@/components/ChatHeader";
 
 export default function ClientChatContent({ user }: { user: SupabaseUser | undefined }) {
-    const { state } = useRoomContext();
-    const { selectedRoom } = state;
+  const { state } = useRoomContext();
+  const { selectedRoom } = state;
 
-    return (
-        <div className="h-[87vh] w-[98vw] lg:w-[50vw] flex flex-col mx-[1em]">
-            <ChatHeader user={user} />
-            <div className="flex-1 flex flex-col">
-                {user && selectedRoom ? (
-                    <>
-                        {/* Messages area - scrollable */}
-                        <div className="flex-1 overflow-y-auto transparent-scrollbar-track">
-                            <ChatMessages />
-                        </div>
-                        {/* Input area - fixed at bottom */}
-                        <div className="flex-shrink-0">
-                            <ChatInput />
-                        </div>
-                    </>
-                ) : (
-                    <ChatAbout />
-                )}
+  return (
+    <div className="h-[87vh] w-full flex flex-col">
+      <ChatHeader user={user} />
+      <div className="flex-1 flex flex-col">
+        {user && selectedRoom ? (
+          <>
+            <div className="flex-1 overflow-y-auto transparent-scrollbar-track">
+              <ChatMessages />
             </div>
-        </div>
-    );
+            <div className="flex-shrink-0">
+              <ChatInput />
+            </div>
+          </>
+        ) : (
+          <ChatAbout />
+        )}
+      </div>
+    </div>
+  );
 }
