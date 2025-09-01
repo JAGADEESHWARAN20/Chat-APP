affa10969f228480108f21a156ad99055c5f617e
JAGADEESHWARAN20 <jagadeeshwaransp5@gmail.com>
2025-09-01 12:29:37 +0530

    Fix async cookies and params issues for Next.js 15+ compatibility

--- components/ThemeTransitionWrapper.tsx
+++ components/ThemeTransitionWrapper.tsx
@@ -60,7 +60,7 @@ export default function ThemeTransitionWrapper({
             initial={{ clipPath: `circle(0% at ${circle.x}px ${circle.y}px)` }}
             animate={{ clipPath: `circle(150% at ${circle.x}px ${circle.y}px)` }}
             exit={{ opacity: 0 }}
-            transition={{ duration: 1.2, ease: "easeInOut" }}
+            transition={{ duration: .5, ease: "easeInOut" }}
             className="fixed inset-0 z-[99999] pointer-events-none"
             style={{
               background:
@@ -76,7 +76,9 @@ export default function ThemeTransitionWrapper({
                 circle.nextTheme === "dark"
                   ? "0 0 0 4px hsl(210 40% 98%)"
                   : "0 0 0 4px hsl(224 71.4% 4.1%)",
-              mixBlendMode: "difference", // keeps border visible against background
+              mixBlendMode: circle.nextTheme === "dark"
+                  ? "screen"
+                  : "multiply", 
             }}
           />
         )}
