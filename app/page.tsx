c8b81c9b0d56012ce237cf5152f5c06c4c4b500c
JAGADEESHWARAN20 <jagadeeshwaransp5@gmail.com>
2025-09-01 23:47:02 +0530

    Fix async cookies and params issues for Next.js 15+ compatibility

--- app/page.tsx
+++ app/page.tsx
@@ -54,12 +54,12 @@ export default function Page() {
             {/* Right section (Actions) */}
             <div className="flex items-center gap-[.25em]">               
                   <Home onClick={() => setActiveTab("home")}
-                  className={`w-[2em] h-[2em] p-[.35em]  flex items-center  ${
-                    activeTab === "home" ? "bg-primary/30 dark:bg-primary/60" : ""
+                  className={`w-[2em] h-[2em] p-[.35em] text-white  flex items-center  ${
+                    activeTab === "home" ? "bg-gray-700 dark:bg-primary/60" : ""
                   }`} />
                   <Search onClick={() => setActiveTab("search")}
-                  className={`w-[2em] h-[2em] p-[.35em] flex items-center ${
-                    activeTab === "search" ? "bg-primary/30 dark:bg-primary/60" : ""
+                  className={`w-[2em] h-[2em] p-[.35em] text-white flex items-center ${
+                    activeTab === "search" ? "bg-gray-700 dark:bg-primary/60" : ""
                   }`} />
               <CreateRoomDialog user={user} />
               <NotificationsWrapper />
