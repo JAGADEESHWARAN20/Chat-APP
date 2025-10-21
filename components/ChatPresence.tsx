8ee384a0cffdcec6125afecaef0fbc2f7c48f20b
JAGADEESHWARAN20 <jagadeeshwaransp5@gmail.com>
2025-10-22 01:55:25 +0530

    tying to fix typing indicator

--- components/ChatPresence.tsx
+++ components/ChatPresence.tsx
@@ -1,27 +1,22 @@
 "use client";
 
 import { useRoomContext } from "@/lib/store/RoomContext";
-import { useActiveUsers } from "@/hooks/useActiveUsers"; // Assuming the path to the hook
+import { RoomActiveUsers } from "./reusable/RoomActiveUsers";
 
 export default function ChatPresence() {
   const { state } = useRoomContext();
   const { selectedRoom } = state;
-
-  const onlineUsers = useActiveUsers(selectedRoom?.id ?? null);
 
   if (!selectedRoom) {
     return <div className="h-3 w-1" />;
   }
 
   return (
-    <div className="flex items-center gap-1 sm:text-[1vw] md:text-[3vw]">
-      <div
-        className={`h-[1.6vw] w-[1.6vw] lg:h-[.6vw] lg:w-[.6vw] rounded-full 
-        ${onlineUsers > 0 ? "bg-green-500 animate-pulse" : "bg-gray-400"}`}
-      />
-      <h1 className="text-[2vw] lg:text-[.3em] text-gray-400">
-        {onlineUsers} {onlineUsers === 1 ? "online" : "online"}
-      </h1>
+    <div className="flex items-center gap-2">
+      <div className="flex items-center gap-1">
+        <div className="h-2 w-2 rounded-full bg-green-500 animate-pulse" />
+        <RoomActiveUsers roomId={selectedRoom.id} />
+      </div>
     </div>
   );
 }
